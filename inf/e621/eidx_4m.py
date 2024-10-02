042b358e6a9cb3fc19da17cc2e23043c1762c3cd
narugo1992 <narugo992@gmail.com>
2024-10-02 16:52:05 +0800

    dev(narugo): x

--- inf/e621/eidx_4m.py
+++ inf/e621/eidx_4m.py
@@ -7,6 +7,6 @@ from .eidx import sync
 if __name__ == '__main__':
     logging.try_init_root(logging.INFO)
     sync(
-        repository=os.environ['deepghs/e621_newest-webp-4Mpixel'],
+        repository=os.environ['REMOTE_REPOSITORY_E621_PUBLIC_4M'],
         exist_repo=os.environ['REMOTE_REPOSITORY_E621_4M_IDX'],
     )
